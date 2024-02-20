fb69023894a9cb003fb551e81bf206b04917545d
WilliamVPT <william.verporte@gmail.com>
2024-02-20 09:50:47 +0100

    structure hypercube

--- hypercube.c
+++ hypercube.c
@@ -7,11 +7,25 @@
 
 typedef struct sommet{
     pid_t pid;
-    int* etiq;
+    int etiq;//identité du sommet
     int pipefd[2];
-    int nb_adj;
-    int* adj;
+    int nb_adj;//nombre de sommetd adjacents
+    int* adj;//tab des adjacents
 }sommet;
+
+//intialise le type sommet
+void init_sommet(sommet *s, int etiq, int n){
+    s->etiq = etiq;
+    s->nb_adj = 0;
+    pipe(s->pipefd);
+    s->adj = (int *)malloc(pow(2,n) * sizeof(int));//tableau de la taille max des processus qui seront créer
+}
+
+//ajout un sommet dans le tableau des adjacent d'un sommet
+void add_adj(sommet *s, int adjacent) {
+    s->adj[s->nb_adj++] = adjacent;
+}
+
 
 int main(int argc, char* argv[]){
     if (argc != 2) {
@@ -20,25 +34,37 @@ int main(int argc, char* argv[]){
     }
     
     int n = atoi(argv[1]); // Convertir l'argument en entier
-    int pipefd[2], process[(int)(pow(2, n))];
-    pid_t pid;
-
+    sommet *sommets = (sommet *) malloc(pow(2,n)*sizeof(sommet));
     for(int i = 0; i < pow(2, n); i++){
-        if(pid=fork() == 0){
-            process[i] = pid;
-            printf("Processus n°%d , pid = %d\n", i, getpid());
-            exit(EXIT_SUCCESS);
+        init_sommet(&sommets[i], i, n);
+    }
+
+    //création structure de l'hypercube
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < pow(n, 2); j++) {
+            int etiq = j ^ (1 << i);
+            if (etiq >= 0 && etiq < pow(2, n)) {
+                add_adj(&sommets[j], sommets[etiq].pipefd[1]);
+            }
         }
     }
-    printf("\n");
-    for(int i = 0; i < pow(2, n); i++) {
-        printf("processus n°%d, pid = %d\n", i, process[i]);    
-    }
+    /* //affichage des sommets et des adjacents
+    for(int i = 0; i<pow(2, n); i++){
+        printf("Sommet : %d\n", sommets[i].etiq);
+        int j = 0;
+        while(j<=pow(2, n) && sommets[i].adj[j]){
+            printf("Sommet adj : %d\n", sommets[i].adj[j]);
+            j++;
+        }
+    }*/
 
-    // Attendre que tous les processus fils se terminent
-    for(int i = 0; i < pow(2, n); i++) {
-        wait(NULL);
+    free(sommets);
+
+    for(int i = 0; i<pow(2, n); i++){
+        free(sommets[i].adj);
     }
+    
+    
 
     return EXIT_SUCCESS;
 }
