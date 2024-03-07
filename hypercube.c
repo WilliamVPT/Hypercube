9fe7642521f734edeb8cc97ef2592d741a32d612
WilliamVPT <william.verporte@gmail.com>
2024-03-07 10:29:50 +0100

    pofinnage

--- hypercube.c
+++ hypercube.c
@@ -1,28 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <string.h>
 #include <math.h>
-#include <signal.h>
 #include <time.h>
 #include <sys/select.h>
 #include <fcntl.h>
 
 #include "token.c"
 
-int main(int argc, char* argv[]){
+int pause_execution = 0;
+pid_t token_pid;
+pid_t main_pid;
+
+void appel_SIGCONT(int sig) {
+    printf("Signal SIGCONT reçu. Processus repris. Appuyez sur 'q' pour arrêter.\n");
+    if (token_pid > 0) {
+        kill(token_pid, SIGCONT);
+        pause_execution = 0;
+    }
+}
+
+void appel_SIGQUIT(int sig) {
+    printf("Arrêt du programme.\n");
+    if (token_pid > 0) {
+        kill(token_pid, SIGKILL); // Terminer le processus token
+    }
+    exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char* argv[]) {
+    signal(SIGCONT, appel_SIGCONT);
+    signal(SIGQUIT, appel_SIGQUIT);
+
+    main_pid = getpid(); // Obtenir le PID du processus principal
+
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <dimension de l'hypercube>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
     
-    int n = atoi(argv[1]); // Convertir l'argument en entier
-    sommet *sommets = (sommet *) malloc(pow(2,n)*sizeof(sommet));
+    int n = atoi(argv[1]);
+    sommet *sommets = (sommet *) malloc(pow(2, n) * sizeof(sommet));
     for(int i = 0; i < pow(2, n); i++){
         init_sommet(&sommets[i], i, n);
     }
 
-    //création structure de l'hypercube
+    // Création de la structure de l'hypercube
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < pow(n, 2); j++) {
             int etiq = j ^ (1 << i);
@@ -32,50 +59,66 @@ int main(int argc, char* argv[]){
         }
     }
 
+
     // Lancement des processus et du token
-    for (int i = 0; i < pow(2, n); i++) {
-        Token(&sommets[i]);
+    token_pid = fork();
+    if (token_pid == 0) {
+        Token(&sommets[0], sommets, pow(2, n));
     }
 
-    int pause = 0; // Flag pour indiquer l'état de pause du processus principal
     printf("Entrez s pour suspendre, r pour reprendre et q pour arrêter\n");
+
+    fd_set fds;
+    struct timeval tv;
+    int stdin_fd = fileno(stdin);
     char touche;
+
     while (1) {
-        touche = getchar();
-        getchar(); // Vider le buffer d'entrée
-        switch (touche) {
-            case 's':
-                // Suspendre le processus principal
-                if (!pause) {
-                    printf("Processus suspendu. Appuyez sur 'r' pour reprendre.\n");
-                    pause = 1;
-                }
-                break;
-            case 'r':
-                // Reprendre le processus principal
-                if (pause) {
-                    printf("Processus repris.\n");
-                    pause = 0;
-                }
-                break;
-            case 'q':
-                // Quitter le programme
-                printf("Arrêt du programme.\n");
-                exit(EXIT_SUCCESS);
-                break;
-            default:
-                // Touche non reconnue
-                printf("Touche non reconnue : %c\n", touche);
-                break;
+        FD_ZERO(&fds);
+        FD_SET(stdin_fd, &fds);
+        tv.tv_sec = 0;
+        tv.tv_usec = 0;
+
+        int ret = select(stdin_fd + 1, &fds, NULL, NULL, &tv);
+
+        if (ret == -1) {
+            perror("select");
+            exit(EXIT_FAILURE);
+        } else if (ret > 0 && FD_ISSET(stdin_fd, &fds)) {
+            touche = getchar();
+            getchar(); // Vider le buffer d'entrée
+
+            switch (touche) {
+                case 's':
+                    // Suspendre les processus fils
+                    for(int i = 0; i<pow(2, n); ++i){
+                        if(getpid() != main_pid && getpid() != sommets[i].pid) // Ne pas suspendre le processus principal
+                            kill(sommets[i].pid, SIGSTOP);
+                    }
+                    break;
+                case 'r':
+                    // Reprendre le processus principal
+                    appel_SIGCONT(SIGCONT);
+                    break;
+                case 'q':
+                    // Quitter le programme
+                    appel_SIGQUIT(SIGQUIT);
+                    break;
+                default:
+                    // Touche non reconnue
+                    printf("Touche non reconnue : %c\n", touche);
+                    break;
+            }
         }
+
+        // Continuer à exécuter d'autres tâches ici si nécessaire
     }
 
-    for(int i = 0; i<pow(2, n); i++){
+    for(int i = 0; i < pow(2, n); i++){
         free(sommets[i].adj);
     }
 
     free(sommets);
 
-    
     return EXIT_SUCCESS;
 }
--- token.c
+++ token.c
@@ -7,13 +7,14 @@
 #include <time.h>
 #include <sys/select.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #include "sommet.c"
 
 #define BUFFER_SIZE 100
 
-
-void Token(sommet *s) { //crée les processus et fait se balader le token
+void Token(sommet *s, sommet *adjacents, int nb_adjacents) {
     pid_t pid;
     pid = fork();
     if (pid == 0) {
@@ -22,7 +23,14 @@ void Token(sommet *s) { //crée les processus et fait se balader le token
         char filename[10];
         sprintf(filename, "%d", s->etiq);
         int fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+        int Log = -1; // Initialiser à -1 pour indiquer que le descripteur de fichier n'est pas encore ouvert
         while (1) {
+            if (Log == -1) {
+                if ((Log = open("Log", O_WRONLY | O_CREAT | O_APPEND, 0644)) == -1) {
+                    perror("Erreur lors de l'ouverture du fichier de log");
+                    exit(EXIT_FAILURE);
+                }
+            }
             // Wait for the token
             fd_set readfds;
             FD_ZERO(&readfds);
@@ -31,7 +39,8 @@ void Token(sommet *s) { //crée les processus et fait se balader le token
             if (FD_ISSET(s->pipefd[0], &readfds)) {
                 char buffer[BUFFER_SIZE];
                 read(s->pipefd[0], buffer, BUFFER_SIZE);
-                // afffichage visite
+                usleep(500000);
+                // affichage visite
                 time_t rawtime;
                 struct tm *timeinfo;
                 time(&rawtime);
@@ -39,15 +48,24 @@ void Token(sommet *s) { //crée les processus et fait se balader le token
                 char timestamp[20];
                 strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", timeinfo);
                 char message[BUFFER_SIZE];
-                sprintf(message, "%s: Token reçu\n", timestamp);
-                write(fd, message, strlen(message));
-                // envoie le token ailleurs aléatoirement
-                int adj = s->adj[rand() % s->nb_adj];
-                write(adj, buffer, BUFFER_SIZE);
+                sprintf(message, "%s: Token reçu à %d\n", timestamp, s->etiq);
+                write(Log, message, strlen(message)); // Écriture dans le fichier de log
+                // envoi le token à un sommet adjacent au hasard
+                int rand_index = rand() % nb_adjacents;
+                sommet adj = adjacents[rand_index];
+                if (Log != -1) {
+                    close(Log); // Fermer le fichier de log si nécessaire
+                    Log = -1; // Réinitialiser le descripteur de fichier
+                }
+                // Appel récursif de Token avec le sommet adjacent
+                Token(&adj, adjacents, nb_adjacents);
             }
         }
+        // Sortir du processus fils si nécessaire
+        exit(EXIT_SUCCESS);
     } else {
         // Processus père
         close(s->pipefd[1]);
+        wait(NULL); // Attendre la fin du processus fils
     }
 }
